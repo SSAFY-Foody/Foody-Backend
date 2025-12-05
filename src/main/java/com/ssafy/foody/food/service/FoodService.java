efda381286f81df4b6c735343b01f9026db32042
ParkJeongHun <mizz1030@naver.com>
2025-12-05 23:01:39 +0900

    Chore: 주석 수정

--- src/main/java/com/ssafy/foody/food/service/FoodService.java
+++ src/main/java/com/ssafy/foody/food/service/FoodService.java
@@ -6,7 +6,7 @@ import com.ssafy.foody.food.dto.FoodResponse;
 
 public interface FoodService {
 	
-//	음식 리스트 조회 (페이지 넘기기) )
+	//	음식 리스트 조회 (페이지 넘기기) )
 	List<FoodResponse> getFoodList(int page, String foodname, String category);
 	
 }
